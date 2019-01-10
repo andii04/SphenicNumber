9304de55f746a6bb19ad4f2e46ed95f539041568
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2019-01-10 17:21:48 +0100

    kjokd

--- component/src/SphenicNumber.java
+++ component/src/SphenicNumber.java
@@ -22,39 +22,29 @@ public class SphenicNumber {
     private ArrayList<BigInteger> calculateSphenicNumber(BigInteger rangeFrom, BigInteger rangeTo)
     {
 
-        /*
-        ArrayList<BigInteger> EvilNumbers = new ArrayList<>();
 
 
-        for(;rangeFrom.compareTo(rangeTo)<=0; rangeFrom = rangeFrom.add(BigInteger.ONE))
-        {
-
-            //if(rangeFrom.mod(BigInteger.TWO) != BigInteger.ZERO)
-            //{
-            BigInteger count = new BigInteger("0");
-            String s = rangeFrom.toString(2);
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i)=='1') {
-                    count = count.add(BigInteger.ONE);
-
-                }
-
-            }
-            if(count.mod(BigInteger.TWO) == BigInteger.ZERO)
-            {
-                EvilNumbers.add(rangeFrom);
-                System.out.println(rangeFrom);
-            }
-
-
-            // }
-
-
-        }
-        */
         return null;
     }
 
+
+    private boolean checkPrime(BigInteger number){
+        //check via BigInteger.isProbablePrime(certainty)
+        if (!number.isProbablePrime(5))
+            return false;
+
+        //check if even
+        BigInteger two = new BigInteger("2");
+        if (!two.equals(number) && BigInteger.ZERO.equals(number.mod(two)))
+            return false;
+
+        //find divisor if any from 3 to 'number'
+        for (BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(number) < 1; i = i.add(two)) { //start from 3, 5, etc. the odd number, and look for a divisor if any
+            if (BigInteger.ZERO.equals(number.mod(i))) //check if 'i' is divisor of 'number'
+                return false;
+        }
+        return true;
+    }
 
     public class Port implements ISphenicNumber {
 
