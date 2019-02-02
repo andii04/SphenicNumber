81559917dcbe2b567c4daed77f157e83a53395df
Fabian Schurk <fabian.schurk@web.de>
2019-02-02 04:26:13 +0100

    glaube es geht

--- component/src/SphenicNumber.java
+++ component/src/SphenicNumber.java
@@ -22,93 +22,25 @@ public class SphenicNumber {
 
     private ArrayList<BigInteger> calculateSphenicNumber(BigInteger rangeFrom, BigInteger rangeTo)
     {
-        BigInteger result=BigInteger.ZERO;
-        BigInteger min = rangeFrom;
-        BigInteger max = rangeTo;
-        ArrayList<BigInteger>b = new ArrayList<>();
         ArrayList<BigInteger>SphenicList = new ArrayList<>();
-
-        for(;min.compareTo(max)<=0;min = min.add(BigInteger.ONE))
-        {
-            if(checkPrime(min)==true)
-            {
-                b.add(min);
-
+        for(;rangeFrom.compareTo(rangeTo)<=0;rangeFrom=rangeFrom.add(BigInteger.ONE)){
+            BigInteger number = rangeFrom;
+            ArrayList<BigInteger>factors = new ArrayList<>();
+            for(BigInteger i = new BigInteger("2"); i.compareTo(number)<=0; i= i.add(BigInteger.ONE)) {
+                while(number.mod(i).compareTo(BigInteger.ZERO)==0) {
+                    if (!factors.contains(i)){
+                        factors.add(i);
+                    }
+                    number = number.divide(i);
+                }
+                if(factors.size()==3){
+                    if(rangeFrom.compareTo(factors.get(0).multiply(factors.get(1).multiply(factors.get(2))))==0){
+                    SphenicList.add(rangeFrom);}
+                }
             }
         }
-
-        BigInteger one = b.get(0);
-        BigInteger two = b.get(1);
-        BigInteger three = b.get(2);
-        //for(BigInteger numb:b)
-        for(int i = 1;i<=b.size()+15;i++)
-        {
-
-
-            result = one.multiply(two).multiply(three);
-            SphenicList.add(result);
-
-            one = b.get(i);
-            two = b.get(i+1);
-            three = b.get(i+1);
-
-
-        }
-        System.out.println(b);
-        System.out.println("");
         System.out.println(SphenicList);
-        System.out.println(result);
         return SphenicList;
-    }
-
-
-    //gibt naechste Zahl zurueck welche eine Primzahl ist
-    private BigInteger getNextPrime(BigInteger number){
-        if(number.mod(BigInteger.TWO).equals(BigInteger.ZERO)) number = number.subtract(BigInteger.ONE);
-        do{
-            number = number.add(BigInteger.TWO);
-        }while(!checkPrime(number));
-
-        return number;
-    }
-
-    //sucht die Distincten Primzahlen und gibt diese zurueck
-    private ArrayList<BigInteger> getDistinctPrimes(BigInteger number){
-        //ArrayList<Integer> distinctPrimes = new ArrayList<>();
-        BigInteger count = new BigInteger("2");
-
-        while (!number.equals(BigInteger.ONE)){
-            if(number.mod(count).equals(BigInteger.ZERO)){
-                number = number.divide(count);
-                if(!distinctPrimes.contains(BigInteger.valueOf(count.intValue()))) distinctPrimes.add(BigInteger.valueOf(count.intValue()));
-
-                continue;
-            }
-
-            count = getNextPrime(count);
-
-        }
-
-        return distinctPrimes;
-    }
-
-    //prueft ob uebergebene Zahl eine Primzahl ist
-    private boolean checkPrime(BigInteger number){
-        //check via BigInteger.isProbablePrime(certainty)
-        if (!number.isProbablePrime(5))
-            return false;
-
-        //check if even
-        BigInteger two = new BigInteger("2");
-        if (!two.equals(number) && BigInteger.ZERO.equals(number.mod(two)))
-            return false;
-
-        //find divisor if any from 3 to 'number'
-        for (BigInteger i = new BigInteger("3"); i.multiply(i).compareTo(number) < 1; i = i.add(two)) { //start from 3, 5, etc. the odd number, and look for a divisor if any
-            if (BigInteger.ZERO.equals(number.mod(i))) //check if 'i' is divisor of 'number'
-                return false;
-        }
-        return true;
     }
 
 
--- src/main/Application.java
+++ src/main/Application.java
@@ -16,7 +16,7 @@ public class Application {
             Method method = port.getClass().getDeclaredMethod("execute", BigInteger.class, BigInteger.class);
             System.out.println(method);
             BigInteger value01 = new BigInteger("1");
-            BigInteger value02 = new BigInteger("50");
+            BigInteger value02 = new BigInteger("5000");
             method.invoke(port,value01,value02);
         } catch (Exception e) {
             e.printStackTrace();
